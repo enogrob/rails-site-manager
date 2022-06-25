9ef3e1699db35e12fe0a0840aabb2c683906d0cc
Roberto Nogueira <enogrob@gmail.com>
2022-06-25 07:55:37 -0300

    refactor-dbs.rebuild_test

--- rebuild_remote_test_db.sh
+++ rebuild_remote_test_db.sh
@@ -10,9 +10,9 @@ echo "[ * } CREATING FRESH TEST DATABASE"
 mysql --user="${DB_USERNAME}" -h ${DB_HOST} --password="${DB_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}; SHOW DATABASES;"
 
 echo "[ * } LOADING TEST DATABASE TABLE STRUCTURE"
-mysql --user="${DB_USERNAME}" -h ${DB_HOST} --password="${DB_PASSWORD}" -e "USE ${DB_NAME};SET FOREIGN_KEY_CHECKS=0;"
-RAILS_ENV=test bundle exec rake db:schema:load
-mysql --user="${DB_USERNAME}" -h ${DB_HOST} --password="${DB_PASSWORD}" -e "USE ${DB_NAME};SET FOREIGN_KEY_CHECKS=1;"
+mysql --user="${DB_USERNAME}" -h ${DB_HOST} --password="${DB_PASSWORD}" -e "USE ${DB_NAME};SET GLOBAL FOREIGN_KEY_CHECKS=0;"
+RAILS_ENV=test db/schema.rbbundle exec rake db:schema:load
+mysql --user="${DB_USERNAME}" -h ${DB_HOST} --password="${DB_PASSWORD}" -e "USE ${DB_NAME};SET GLOBAL FOREIGN_KEY_CHECKS=1;"
 
 echo "[ * } RUNNING ANY NEW DATABASE MIGRATIONS"
 RAILS_ENV=test bundle exec rake db:migrate
